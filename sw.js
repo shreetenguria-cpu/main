842341ce6405c012b00caea79a244ec42af35e0e
shreetenguria-cpu <shreetenguria@gmail.com>
2026-01-22 01:36:39 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,6 +6,10 @@ self.addEventListener("install",e=>{
   );
 });
 
+window.onload = () => {
+  setBrand('cafe', document.querySelector('.brand-btn'));
+};
+
 self.addEventListener("fetch",e=>{
   e.respondWith(
     caches.match(e.request).then(res=>res||fetch(e.request))
